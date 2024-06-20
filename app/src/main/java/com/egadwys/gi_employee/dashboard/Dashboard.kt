8ef8d10d9b5b6ccbc6dc8c9d51fd6f2612ceee22
Egadwys <egadwys@gmail.com>
2024-06-20 10:58:35 +0700

    update: update colors, add navigation drawer

--- app/src/main/java/com/egadwys/gi_employee/dashboard/Dashboard.kt
+++ app/src/main/java/com/egadwys/gi_employee/dashboard/Dashboard.kt
@@ -5,18 +5,24 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.video.VideoRecordEvent.Start
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager2.widget.ViewPager2
 import com.egadwys.gi_employee.R
 import com.egadwys.gi_employee.dashboard.viewpager.ViewPagerAdapter
 import com.egadwys.gi_employee.payroll.detail.Detail
+import com.google.android.material.navigation.NavigationView
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class Dashboard : AppCompatActivity() {
@@ -26,10 +32,24 @@ class Dashboard : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var dashboard_header: TextView
     private lateinit var dashboard_logout: ImageView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
+
+        var cnb = 0
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         dashboard_header = findViewById(R.id.dashboard_header)
         dashboard_logout = findViewById(R.id.dashboard_logout)
@@ -52,12 +72,39 @@ class Dashboard : AppCompatActivity() {
             when (id) {
                 R.id.menu_dashboard -> {
                     viewPager.currentItem = 0
+                    cnb = 0
                 }
                 R.id.menu_attendance -> {
                     viewPager.currentItem = 1
+                    cnb = 1
+                }
+                R.id.menu_profile -> {
+                    drawerLayout.openDrawer(GravityCompat.START)
                 }
             }
         }
+
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerSlide(drawerView: android.view.View, slideOffset: Float) {
+                // Respond when the drawer's position changes
+            }
+
+            override fun onDrawerOpened(drawerView: android.view.View) {
+                // Respond when the drawer is opened
+            }
+
+            override fun onDrawerClosed(drawerView: android.view.View) {
+                if (cnb == 0) {
+                    menu.setItemSelected(R.id.menu_dashboard, true)
+                } else {
+                    menu.setItemSelected(R.id.menu_attendance, true)
+                }
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+                // Respond when the drawer motion state changes
+            }
+        })
 
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
@@ -65,9 +112,11 @@ class Dashboard : AppCompatActivity() {
                 // Handle page selection
                 when (position) {
                     0 -> {
+                        cnb = 0
                         menu.setItemSelected(R.id.menu_dashboard, true)
                     }
                     1 -> {
+                        cnb = 1
                         menu.setItemSelected(R.id.menu_attendance, true)
                     }
                 }
@@ -106,10 +155,14 @@ class Dashboard : AppCompatActivity() {
     }
 
     private fun shouldHandleBackPress(): Boolean {
-        if (viewPager.currentItem == 0){
-            finishAffinity()
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers()
         } else {
-            viewPager.currentItem = 0
+            if (viewPager.currentItem == 0) {
+                finishAffinity()
+            } else {
+                viewPager.currentItem = 0
+            }
         }
         return true
     }
